0c741b87f457a6e48c3ed155ef3e4fa148d06964
Ali Mehdi <salimehdi144@gmail.com>
2018-12-29 21:47:39 +0500

    Update datapreprocessing.py

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -35,3 +35,9 @@ y = labelencoder_Y.fit_transform(Y)
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 0)
 
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
